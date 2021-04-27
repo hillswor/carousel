81e44f9641706fa8fd9746263473b549a464c721
Bruce Hillsworth <brucehillsworth@Air.local>
2021-04-27 17:05:01 -0600

    Added Javascript

---
+++ index.js
@@ -0,0 +1,23 @@
+const slides = document.getElementsByClassName("carousel-item")
+
+let slidePosition = 0
+
+const totalSlides = slides.length
+
+const nextButton = document.getElementById("carousel-button-next")
+
+const prevButton = document.getElementById("carousel-button-prev")
+
+function moveToPrevSlide() {
+
+}
+
+function moveToNextSlide() {
+
+}
+
+prevButton.addEventListener("click", moveToPrevSlide())
+
+nextButton.addEventListener("click", moveToNextSlide())
+
+console.log(slides)
