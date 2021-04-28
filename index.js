199a58e04a5913ce1cef61c4c72be934471a15e5
Bruce Hillsworth <brucehillsworth@Air.local>
2021-04-27 20:41:20 -0600

    Finished Carousel

--- index.js
+++ index.js
@@ -8,16 +8,41 @@ const nextButton = document.getElementById("carousel-button-next")
 
 const prevButton = document.getElementById("carousel-button-prev")
 
-function moveToPrevSlide() {
 
+function hideAllSlides() {
+	for (const slide of slides) {
+		slide.classList.remove("carousel-item-visible")
+		slide.classList.add("carousel-item-hidden")
+	}
 }
+
+
+function moveToPrevSlide() {
+	// slides[slidePosition].classList.remove("carousel-item-visible")
+	hideAllSlides()
+	if (slidePosition === 0) {
+		slidePosition = totalSlides - 1
+	} else {
+		slidePosition --
+	}
+
+	slides[slidePosition].classList.add("carousel-item-visible")
+}
+
 
 function moveToNextSlide() {
+	// slides[slidePosition].classList.remove("carousel-item-visible")
+	hideAllSlides()
+	if (slidePosition === totalSlides - 1) {
+		slidePosition = 0
+	} else {
+		slidePosition ++
+	}
 
+	slides[slidePosition].classList.add("carousel-item-visible")
 }
+
 
 prevButton.addEventListener("click", moveToPrevSlide)
 
 nextButton.addEventListener("click", moveToNextSlide)
-
-console.log(slides)
